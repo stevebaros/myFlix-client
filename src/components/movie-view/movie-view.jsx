130bfce0e00fbe9cbde892ca047ba510d138e1fd
Lisa Clement <lisa.clement.a@gmail.com>
2022-04-13 17:35:10 +0200

    import button, badge and browserRouter + add some fixes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,44 +1,73 @@
 import React from "react";
 import Axios from "axios";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Badge from "react-bootstrap/Badge";
 
 export class MovieView extends React.Component {
   render() {
     const { movie, onBackClick } = this.props;
 
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
+      <>
+        <div>
+          <Button
+            variant="outline-light"
+            onClick={() => {
+              onBackClick();
+            }}
+          >
+            Back
+          </Button>
         </div>
+
         <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
+          <h1 className="display-4">{movie.Title}</h1>
         </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
+        <div className="movie-img text-left">
+          <img src={movie.ImagePath} width="350" className="img-fluid" />
+        </div>
+
+        <div>
+          <Link to={`/genres/${movie.Genre.Name}`} className="d-inline-flex">
+            <Badge pill bg="light" text="dark">
+              {movie.Genre.Name}
+            </Badge>
+          </Link>
+        </div>
+        <div>
+          <p>Director: </p>
+          <Link to={`/directors/${movie.Director.Name}`} className="d-inline-flex">
+            {movie.Director.Name}
+          </Link>
+        </div>
+
+        <br />
+        <div>
           <span className="value">{movie.Description}</span>
         </div>
-        <span className="movie-view__line__label">Genre: </span>
-        <Link to={`/genres/${movie.Genre.Name}`}>
-          <span className="movie-view__line__value">{movie.Genre.Name}</span>
-        </Link>
-        <span className="movie-view__line__label">Director: </span>
-        <Link to={`/directors/${movie.Director.Name}`}>
-          <span className="movie-view__line__value">{movie.Director.Name}</span>
-        </Link>
 
-        <button
-          onClick={() => {
-            onBackClick(null);
-          }}
-        >
-          Back
-        </button>
-      </div>
+        <Link to={"/"}>
+          <Button variant="outline-light">Back to full list</Button>
+        </Link>
+      </>
+      // onClick() event listener sets selectedMovie variable in main-view to null, allowing to return back to list of MovieCards
     );
   }
 }
+
+/* Use propTypes to validate data types of props
+    Validation logic:
+    movie object is required, if object contains a title, the Title has to be a string
+    onBackClick function is required
+*/
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string,
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired,
+};
