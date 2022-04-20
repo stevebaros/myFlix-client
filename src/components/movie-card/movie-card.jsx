77207158dc0b23730acceed15ffaa19536283ae2
Lisa Clement <lisa.clement.a@gmail.com>
2022-04-20 12:34:35 +0200

    Add button to add movie to favorite movielist

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,13 +5,35 @@ import Card from "react-bootstrap/Card";
 import { FavoriteMovies } from "../profile-view/favorite-movies";
 
 import { Link } from "react-router-dom";
+const axios = require("axios").default;
 
 export class MovieCard extends React.Component {
+  addFavoriteMovie() {
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+
+    axios
+      .post(
+        `https://give-me-movies.herokuapp.com/users/${user}/movies/${this.props.movie._id}`,
+        {},
+        {
+          headers: { Authorization: `Bearer ${token}` },
+          method: "POST",
+        }
+      )
+      .then((response) => {
+        alert(`Added ${this.props.movie.Title} to your favorites!`);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   render() {
     const { movie } = this.props;
 
     return (
-      <Card>
+      <Card className="d-flex  justify-content-center">
         <Card.Img variant="top" src={movie.ImagePath} />
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
@@ -19,9 +41,9 @@ export class MovieCard extends React.Component {
           <Link to={`/movies/${movie._id}`}>
             <Button variant="link">Open</Button>
           </Link>
-          <Button type="button" variant="outline-primary" onClick={() => FavoriteMovies()}>
-            {" "}
-            Fav{" "}
+
+          <Button variant="info" value={movie._id} onClick={(e) => this.addFavoriteMovie(e, movie)}>
+            Add to Fav
           </Button>
         </Card.Body>
       </Card>
